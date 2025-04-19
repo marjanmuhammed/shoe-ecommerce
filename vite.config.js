dc4cd5dd72bc34bfe6c845d9d9665e4517a6d723
marjanmuhammed <marjanmuhammad790@gmail.com>
2025-04-19 12:54:13 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite';
+
 
 
 // https://vite.dev/config/
@@ -9,9 +9,4 @@ export default defineConfig({
   plugins: [react(),    tailwindcss(),
   ],
   
-export default defineConfig({
-  preview: {
-    allowedHosts: ['json-sever-mru6.onrender.com', 'localhost', '0.0.0.0']
-  }
-});
 })  
