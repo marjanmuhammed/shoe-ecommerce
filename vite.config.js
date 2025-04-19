8b4a551b19e5709177da2dab289dc0865ac541a7
marjanmuhammed <marjanmuhammad790@gmail.com>
2025-04-19 12:33:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),    tailwindcss(),
   ],
-  server:{
-    watch:{
-      ignored:["**/db.json"]
-    }
-
-
+  
+export default defineConfig({
+  preview: {
+    allowedHosts: ['json-sever-mru6.onrender.com', 'localhost', '0.0.0.0']
   }
+});
 })  
